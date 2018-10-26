a7affab87aafe43eccd0156f330796082a0c497b
taaviv <taavi.vreimann@reach-u.com>
2018-10-26 20:41:02 +0300

    configurable swagger path

--- src/main/java/au/property/mgmt/rest/conf/SwaggerConf.java
+++ src/main/java/au/property/mgmt/rest/conf/SwaggerConf.java
@@ -1,5 +1,6 @@
 package au.property.mgmt.rest.conf;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -15,13 +16,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConf {
 
+    @Value("${swagger.base.path:}")
+    private String basePath;
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .pathMapping(basePath);
     }
 
 }
