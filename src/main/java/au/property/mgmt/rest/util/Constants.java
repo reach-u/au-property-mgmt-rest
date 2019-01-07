fdb494de8211fb290ebc34e6057aa2645a944e3c
Sander Vunk <sander.vunk@reach-u.com>
2019-01-07 17:21:51 +0200

    RUSS-48 DTO for payments, payment grouping

--- src/main/java/au/property/mgmt/rest/util/Constants.java
+++ src/main/java/au/property/mgmt/rest/util/Constants.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Constants {
 
-    public static final String API_V1_URL = "api/1";
+    public static final String API_V1_URL = "au-property-mgmt-rest/api/1";
 
     public static final String PRIVATE_API_V1_URL = API_V1_URL + "/priv";
 
